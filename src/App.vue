6b59fce477f23be4434056a562e89e7d45ee6822
hearsay316 <hearsay316@gmail.com>
2019-08-27 14:49:26 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,62 @@
 export default {
   data() {
     return {
-
+      options: [
+        {
+          label: '肉类',
+          children: [
+            {
+              label: '猪肉',
+              children: [
+                {
+                  label: '五花肉',
+                },
+                {
+                  label: '里脊肉',
+                },
+              ],
+            },
+            {
+              label: '鸡肉',
+              children: [
+                {
+                  label: '鸡腿',
+                },
+                {
+                  label: '鸡翅',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: '蔬菜',
+          children: [
+            {
+              label: '叶菜类',
+              children: [
+                {
+                  label: '大白菜',
+                },
+                {
+                  label: '小白菜',
+                },
+              ],
+            },
+            {
+              label: '根茎类',
+              children: [
+                {
+                  label: '萝卜',
+                },
+                {
+                  label: '土豆',
+                },
+              ],
+            },
+          ],
+        },
+      ],
     };
   },
 };
